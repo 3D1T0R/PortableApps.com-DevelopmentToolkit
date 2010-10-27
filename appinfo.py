53c670ea90e961b981b79150d792080d2dbdf1bb
Chris Morgan <chriswombat@users.sourceforge.net>
2010-10-27 15:32:43 +1100

    Basic appinfo.ini editing UI (but no control or way of running it yet).

---
+++ appinfo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from PyQt4.QtGui import QDialog
+from ui.appinfo import Ui_AppInfoDialog
+from utils import center_window
+
+class AppInfoDialog(QDialog):
+    def __init__(self, parent=None):
+        super(AppInfoDialog, self).__init__(parent)
+        self.ui=Ui_AppInfoDialog()
+        self.ui.setupUi(self)
+
+def show():
+    window = AppInfoDialog()
+    center_window(window)
+    window.show()
+    return window
--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 pyrcc4 graphics/graphics.qrc -o ui/graphics_rc.py
 pyuic4 ui/mainwindow.ui -o ui/mainwindow.py
+pyuic4 ui/appinfo.ui -o ui/appinfo.py
 pyuic4 ui/validationsimple.ui -o ui/validationsimple.py
