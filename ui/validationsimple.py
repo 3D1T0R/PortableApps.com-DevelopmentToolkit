71cb99e38ff35b1576e116c8ef25dee4cc54177e
Chris Morgan <chriswombat@users.sourceforge.net>
2010-10-28 13:43:24 +1100

    AppInfo GUI: made dropdown options non-translateable and named a few controls.

--- ui/validationsimple.py
+++ ui/validationsimple.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/validationsimple.ui'
 #
-# Created: Mon Oct 25 17:46:24 2010
+# Created: Thu Oct 28 13:41:58 2010
 #      by: PyQt4 UI code generator 4.7.4
 #
 # WARNING! All changes made in this file will be lost!
