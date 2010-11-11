a6781b300b07cbb774367e242327b3302bd23c85
Chris Morgan <chriswombat@users.sourceforge.net>
2010-11-11 23:21:01 +1100

    Removed old and useless languages.english module.

--- languages/english.py
+++
@@ -1,8 +0,0 @@
-import languages
-
-class English(languages.Language):
-    LANGUAGE_NAME = 'English'
-    LANGUAGE_NAME_NATIVE = 'English'
-    # All string defaults are defined in Language as it's the default
-
-language = English()
