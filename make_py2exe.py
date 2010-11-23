bb6ff95ddc2a7a70461fe40f80bacd970c8b8ae2
Chris Morgan <chriswombat@users.sourceforge.net>
2010-11-23 13:23:57 +1100

    More complete make_py2exe.py, copying all that's required for immediate running.

--- make_py2exe.py
+++ make_py2exe.py
@@ -3,6 +3,36 @@ import py2exe
 import os
 import re
 import sys
+import glob
+from collections import defaultdict
+
+
+def find_data_files(source, target, patterns):
+    """
+    Locates the specified data-files and returns the matches in a data_files
+    compatible format.
+
+    source is the root of the source data tree.
+        Use '' or '.' for current directory.
+    target is the root of the target data tree.
+        Use '' or '.' for the distribution directory.
+    patterns is a sequence of glob-patterns for the
+        files you want to copy.
+
+    Modified slightly from http://www.py2exe.org/index.cgi/data_files
+    """
+    if glob.has_magic(source) or glob.has_magic(target):
+        raise ValueError("Magic not allowed in src, target")
+    ret = defaultdict(list)
+    for pattern in patterns:
+        pattern = os.path.join(source, pattern)
+        for filename in glob.glob(pattern):
+            if os.path.isfile(filename):
+                targetpath = os.path.join(target, os.path.relpath(filename,
+                    source))
+                ret[os.path.dirname(targetpath)].append(filename)
+    return sorted(ret.items())
+
 
 version = sys.argv[-1]
 if re.findall('^([0-9]+\.)*[0-9]+(b[0-9]+)?$', version):
@@ -38,10 +68,24 @@ manifest = '''
 </assembly>
 '''
 
+data_files = find_data_files('', '', [
+    # No need for 'images/*.png' etc. yet due to the QRC solution
+    'languages/*.ini',
+    'app-template/*',
+    'app-template/*/*',
+    'app-template/*/*/*',
+    'app-template/*/*/*/*',
+    # Scary. (Or rather, lazy!)
+])
+data_files += find_data_files(os.path.dirname(sys.executable), '', [
+    'msvcr90.dll', 'Microsoft.VC90.CRT.manifest'])
+
 # options['py2exe']['bundle_files']=1 and zipfile=None make it compress less
 # well with UPX, so they're not turned on.
 setup(
     options={'py2exe': dict(compressed=1, optimize=1, includes=['sip'])},
+    data_files=data_files,
+
     windows=[dict(
         script='main.py',
         icon_resources=[(1, 'graphics/appicon.ico')],
