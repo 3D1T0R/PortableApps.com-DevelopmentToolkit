af229ebd764fb9b8a0670c2a93569e9a05ab74c6
Chris Morgan <chriswombat@users.sourceforge.net>
2011-06-06 15:23:59 +1000

    Updated valid languages.

--- paf/__init__.py
+++ paf/__init__.py
@@ -9,16 +9,16 @@ CATEGORIES = ('Accessibility', 'Development', 'Education', 'Games',
 LANGUAGES = ('Multilingual', 'Afrikaans', 'Albanian', 'Arabic', 'Armenian',
         'Basque', 'Belarusian', 'Bosnian', 'Breton', 'Bulgarian', 'Catalan',
         'Cibemba', 'Croatian', 'Czech', 'Danish', 'Dutch', 'Efik', 'English',
-        'Estonian', 'Farsi', 'Finnish', 'French', 'Galician', 'Georgian',
-        'German', 'Greek', 'Hebrew', 'Hungarian', 'Icelandic', 'Igbo',
-        'Indonesian', 'Irish', 'Italian', 'Japanese', 'Khmer', 'Korean',
-        'Kurdish', 'Latvian', 'Lithuanian', 'Luxembourgish', 'Macedonian',
-        'Malagasy', 'Malay', 'Mongolian', 'Norwegian', 'NorwegianNynorsk',
-        'Pashto', 'Polish', 'Portuguese', 'PortugueseBR', 'Romanian',
-        'Russian', 'Serbian', 'SerbianLatin', 'SimpChinese', 'Slovak',
-        'Slovenian', 'Spanish', 'SpanishInternational', 'Swahili', 'Swedish',
-        'Thai', 'TradChinese', 'Turkish', 'Ukranian', 'Uzbek', 'Valencian',
-        'Vietnamese', 'Welsh', 'Yoruba')
+        'EnglishGB', 'Esperanto', 'Estonian', 'Farsi', 'Finnish', 'French',
+        'Galician', 'Georgian', 'German', 'Greek', 'Hebrew', 'Hungarian',
+        'Icelandic', 'Igbo', 'Indonesian', 'Irish', 'Italian', 'Japanese',
+        'Khmer', 'Korean', 'Kurdish', 'Latvian', 'Lithuanian', 'Luxembourgish',
+        'Macedonian', 'Malagasy', 'Malay', 'Mongolian', 'Norwegian',
+        'NorwegianNynorsk', 'Pashto', 'Polish', 'Portuguese', 'PortugueseBR',
+        'Romanian', 'Russian', 'Serbian', 'SerbianLatin', 'SimpChinese',
+        'Slovak', 'Slovenian', 'Spanish', 'SpanishInternational', 'Swahili',
+        'Swedish', 'Thai', 'TradChinese', 'Turkish', 'Ukrainian', 'Uzbek',
+        'Valencian', 'Vietnamese', 'Welsh', 'Yoruba')
 
 
 class PAFException(Exception):
