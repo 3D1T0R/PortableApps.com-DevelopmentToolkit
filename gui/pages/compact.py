f92a99c59bdd55ca169d7716e109f313ba7c59f4
Chris Morgan <chriswombat@users.sourceforge.net>
2011-06-10 22:39:30 +1000

    AppCompactor implemented in the GUI (except for an edit control for CompressionFileSizeCutOff).

--- gui/pages/compact.py
+++ gui/pages/compact.py
@@ -1,6 +1,38 @@
 from ._base import WindowPage
 from ..ui.pagecompact import Ui_PageCompact
+from paf import AppCompactor
+from PyQt4.QtCore import Slot
 
 
 class PageCompact(WindowPage, Ui_PageCompact):
-    pass
+    def enter(self):
+        self.appcompactor = AppCompactor(self.window.package)
+        self.appcompactor.load()
+        self.files_excluded.setText('|'.join(self.appcompactor.files_excluded))
+        self.additional_extensions_included.setText('|'.join(self.appcompactor.additional_extensions_included))
+        self.additional_extensions_excluded.setText('|'.join(self.appcompactor.additional_extensions_excluded))
+
+    def leave(self, closing=True):
+        self.save()
+
+    @Slot()
+    def on_start_button_clicked(self):
+        if self.window.page_options.find_appcompactor_path():
+            self.save()  # Necessary while running an external process
+            self.appcompactor.compact(block=False)
+        # If not, the user was told about it in find_appcompactor_path
+
+    def save(self):
+        self.appcompactor.files_excluded = self.files_excluded.text().split('|')
+        if self.appcompactor.files_excluded == ['']:
+            del self.appcompactor.files_excluded[0]
+
+        self.appcompactor.additional_extensions_included = self.additional_extensions_included.text().split('|')
+        if self.appcompactor.additional_extensions_included == ['']:
+            del self.appcompactor.additional_extensions_included[0]
+
+        self.appcompactor.additional_extensions_excluded = self.additional_extensions_excluded.text().split('|')
+        if self.appcompactor.additional_extensions_excluded == ['']:
+            del self.appcompactor.additional_extensions_excluded[0]
+
+        self.appcompactor.save()
--- paf/appcompactor.py
+++ paf/appcompactor.py
@@ -7,7 +7,7 @@ import sys
 from subprocess import Popen, PIPE
 from orderedset import OrderedSet
 import config
-from utils import win32
+from utils import win32, get_ini_str
 from validator.engine import INIManager, SectionValidator, FileMeta, SectionMeta, ValidatorError, StringMapping
 
 
@@ -30,17 +30,21 @@ class AppCompactor(INIManager):
                 StringMapping('PortableApps.comAppCompactor', 'Section'),
                 )
 
-    def compact(self):
+    def compact(self, block=True):
         """
         Compacts a package with the PortableApps.com AppCompactor. Currently
         this runs the normal GUI (with the path filled in) and the user will
         still need to press "Go".
 
-        Raises an ``OSError`` if run on Linux/OS X and Wine is not installed.
+        Raises an ``OSError`` if run on Linux/OS X and Wine is not installed or
+        PortableApps.comAppCompactor.exe does not have mode +x.
 
-        Returns True on success, or False if the PortableApps.com AppCompactor
-        was not found. If something went otherwise wrong, you probably won't
-        hear about it.
+        Returns the process handle on success, or False if the PortableApps.com
+        AppCompactor was not found. If something went otherwise wrong, you
+        probably won't hear about it.
+
+        The parameter ``block`` can be set to ``False`` to make this an
+        asynchronous call.
         """
 
         appcompactor_path = config.get('Main', 'AppCompactorPath')
@@ -56,9 +60,59 @@ class AppCompactor(INIManager):
             package_path = Popen(['winepath', '-w', package_path],
                     stdout=PIPE).communicate()[0].strip()
 
-        Popen([appcompactor_path, package_path]).wait()
+        proc = Popen([appcompactor_path, package_path])
+        if block:
+            proc.wait()
 
-        return True
+        return proc
+
+    def load(self, do_reload=True):
+        if not do_reload and self.ini:
+            return
+
+        super(AppCompactor, self).load(do_reload)
+
+        self.files_excluded = get_ini_str(self.ini,
+            'PortableApps.comAppCompactor', 'FilesExcluded', '').split('|')
+        if self.files_excluded == ['']:
+            del self.files_excluded[0]
+
+        self.additional_extensions_included = get_ini_str(self.ini,
+            'PortableApps.comAppCompactor', 'AdditionalExtensionsIncluded', '').split('|')
+        if self.additional_extensions_included == ['']:
+            del self.additional_extensions_included[0]
+
+        self.additional_extensions_excluded = get_ini_str(self.ini,
+            'PortableApps.comAppCompactor', 'AdditionalExtensionsExcluded', '').split('|')
+        if self.additional_extensions_excluded == ['']:
+            del self.additional_extensions_excluded[0]
+
+    def save(self):
+        # TODO: rethink slightly the INIManager.fix() method which is more what
+        # some of this should be. Perhaps def save(self, fix=True)?
+        if self.files_excluded:
+            self.ini['PortableApps.comAppCompactor'].FilesExcluded = '|'.join(self.files_excluded)
+        elif 'FilesExcluded' in self.ini['PortableApps.comAppCompactor']:
+            del self.ini['PortableApps.comAppCompactor'].FilesExcluded
+
+        if self.additional_extensions_included:
+            self.ini['PortableApps.comAppCompactor'].AdditionalExtensionsIncluded = '|'.join(self.additional_extensions_included)
+        elif 'AdditionalExtensionsIncluded' in self.ini['PortableApps.comAppCompactor']:
+            del self.ini['PortableApps.comAppCompactor'].AdditionalExtensionsIncluded
+
+        if self.additional_extensions_excluded:
+            self.ini['PortableApps.comAppCompactor'].AdditionalExtensionsExcluded = '|'.join(self.additional_extensions_excluded)
+        elif 'AdditionalExtensionsExcluded' in self.ini['PortableApps.comAppCompactor']:
+            del self.ini['PortableApps.comAppCompactor'].AdditionalExtensionsExcluded
+
+        if 'PortableApps.comAppCompactor' in self.ini and len(self.ini['PortableApps.comAppCompactor']) == 0:
+            del self.ini['PortableApps.comAppCompactor']
+
+        # This is cheating, really. See note at top of method.
+        if len(self.ini) == 0:
+            self.delete()
+        else:
+            super(AppCompactor, self).save()
 
 
 class Section(SectionValidator):
