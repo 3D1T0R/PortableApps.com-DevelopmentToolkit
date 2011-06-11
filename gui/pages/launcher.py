270ef84da818976266b091b50b8287aa60823044
Chris Morgan <chriswombat@users.sourceforge.net>
2011-06-12 00:03:05 +1000

    Added a "Build Launcher" button to the Launcher page.

--- gui/pages/launcher.py
+++ gui/pages/launcher.py
@@ -1,6 +1,22 @@
-from ._base import WindowPage
+from ._base import WindowPage, assert_valid_package_path
 from ..ui.pagelauncher import Ui_PageLauncher
+from PyQt4 import QtCore, QtGui
+from utils import _
 
 
 class PageLauncher(WindowPage, Ui_PageLauncher):
-    pass
+    @QtCore.Slot()
+    @assert_valid_package_path
+    def on_build_launcher_clicked(self):
+        # First of all, check that it's valid.
+        if self.window.package.appid is None:
+            QtGui.QMessageBox.critical(self,
+                    _('PortableApps.com Development Toolkit'),
+                    _('The package AppID must be set before you can compile the launcher.'),
+                    QtGui.QMessageBox.Ok)
+            self.window.set_page('details')
+            return
+
+        if self.window.page_options.find_launcher_path():
+            # No callback at present, use QThread when we need it
+            self.window.package.launcher.build(block=False)
--- gui/ui/pagelauncher.py
+++ gui/ui/pagelauncher.py
@@ -14,15 +14,22 @@ class Ui_PageLauncher(object):
         PageLauncher.resize(960, 445)
         self.verticalLayout = QtGui.QVBoxLayout(PageLauncher)
         self.verticalLayout.setObjectName("verticalLayout")
+        self.build_launcher = QtGui.QCommandLinkButton(PageLauncher)
+        self.build_launcher.setObjectName("build_launcher")
+        self.verticalLayout.addWidget(self.build_launcher)
         self.label_6 = QtGui.QLabel(PageLauncher)
         self.label_6.setTextFormat(QtCore.Qt.AutoText)
         self.label_6.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_6.setOpenExternalLinks(True)
+        self.label_6.setTextInteractionFlags(QtCore.Qt.LinksAccessibleByKeyboard|QtCore.Qt.LinksAccessibleByMouse)
         self.label_6.setObjectName("label_6")
         self.verticalLayout.addWidget(self.label_6)
+        self.verticalLayout.setStretch(1, 1)
 
         self.retranslateUi(PageLauncher)
         QtCore.QMetaObject.connectSlotsByName(PageLauncher)
 
     def retranslateUi(self, PageLauncher):
-        self.label_6.setText(QtGui.QApplication.translate("PageLauncher", "<p>A really advanced sort of editor and manager for PortableApps.com Launcher configuration will go here.</p><p>Unfortunately, it\'s going to be <em>so</em> powerful that it\'s not ready yet.</p>", None, QtGui.QApplication.UnicodeUTF8))
+        self.build_launcher.setText(QtGui.QApplication.translate("PageLauncher", "&Build launcher", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_6.setText(QtGui.QApplication.translate("PageLauncher", "<p>A really advanced sort of editor and manager for PortableApps.com Launcher configuration will go here.</p><p>Unfortunately, it\'s going to be <em>so</em> powerful that it\'s not ready yet.</p><p>In the mean time, refer to the <a href=\"http://portableapps.com/manuals/PortableApps.comLauncher/\">PortableApps.com Launcher manual</a> for information on what to do.</p>", None, QtGui.QApplication.UnicodeUTF8))
 
