252305f0a0ea2b506afa6d272e54bd7370e4c2e5
3D1T0R <3D1T0R@users.noreply.github.com>
2016-03-18 12:49:10 -0700

    Allow the Browse buttons on the Options page to work even if the file the user is browsing for has already been found (user may have multiple copies of something)

--- gui/pages/options.py
+++ gui/pages/options.py
@@ -7,12 +7,12 @@ from utils import _, path_local
 
 
 def _options_path_finder(thing):
-    def wrapped(self, silent=False):
+    def wrapped(self, mode=None):
         path = config.get('Main', thing + 'Path')
-        if silent and path is not None:
+        if mode == 'silent' and path is not None:
             return path
 
-        if not path or not os.path.isfile(path):
+        if mode == 'browse' or not path or not os.path.isfile(path):
             # Try intelligently guessing if in PAF structure
             portableapps_dir = os.path.dirname(
                     config.ROOT_DIR.rpartition(
@@ -24,9 +24,9 @@ def _options_path_finder(thing):
                 path = os.path.join(portableapps_dir,
                     'PortableApps.com' + thing,
                     'PortableApps.com%sGenerator.exe' % thing)
-            if silent:
+            if mode == 'silent':
                 return path if portableapps_dir and os.path.isfile(path) else ''
-            if not portableapps_dir or not os.path.isfile(path):
+            if mode == 'browse' or not portableapps_dir or not os.path.isfile(path):
                 path = path_local(QtGui.QFileDialog.getOpenFileName(self,
                         _('Select the path to the PortableApps.com ' + thing),
                         config.ROOT_DIR,
@@ -39,7 +39,7 @@ def _options_path_finder(thing):
 
             if path and os.path.isfile(path):
                 config.settings.Main[thing + 'Path'] = path
-            else:
+            elif not mode == 'browse':
                 QtGui.QMessageBox.critical(self,
                         _('PortableApps.com Development Toolkit'),
                         _('Unable to locate the PortableApps.com %s.' % thing),
@@ -56,9 +56,9 @@ class PageOptions(WindowPage, Ui_PageOptions):
     find_appcompactor_path = _options_path_finder('AppCompactor')
 
     def enter(self):
-        self.installer.setText(self.find_installer_path(True))
-        self.launcher.setText(self.find_launcher_path(True))
-        self.appcompactor.setText(self.find_appcompactor_path(True))
+        self.installer.setText(self.find_installer_path('silent'))
+        self.launcher.setText(self.find_launcher_path('silent'))
+        self.appcompactor.setText(self.find_appcompactor_path('silent'))
 
     def leave(self, closing=False):
         config.settings.Main.InstallerPath = self.installer.text()
@@ -70,18 +70,18 @@ class PageOptions(WindowPage, Ui_PageOptions):
         """PortableApps.com Installer Path."""
         text_box = self.installer
         current_path = text_box.text()
-        text_box.setText(self.find_installer_path() or current_path)
+        text_box.setText(self.find_installer_path('browse') or current_path)
 
     @QtCore.Slot()
     def on_launcher_browse_clicked(self):
         """PortableApps.com Launcher Path."""
         text_box = self.launcher
         current_path = text_box.text()
-        text_box.setText(self.find_launcher_path() or current_path)
+        text_box.setText(self.find_launcher_path('browse') or current_path)
 
     @QtCore.Slot()
     def on_appcompactor_browse_clicked(self):
         """PortableApps.com AppCompactor Path."""
         text_box = self.appcompactor
         current_path = text_box.text()
-        text_box.setText(self.find_appcompactor_path() or current_path)
+        text_box.setText(self.find_appcompactor_path('browse') or current_path)
